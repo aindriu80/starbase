438696b6f474d08d2d656d1baf32eafd6e2e3157
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-10-17 21:27:47 +0100

    The Star Wars API - SWAPI with Axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
+ 
+
 <template>
 <div id="app">
     <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
 </div>
 </template>
 
@@ -11,6 +14,20 @@ export default {
     return {
       title: "Generate Your Team"
     };
+  },
+  methods: {
+    fetchCharacter() {
+      fetch("http://swapi.com/api/people", {
+        mode: "no-cors",
+        method: "GET"
+      });
+      // .then(response => response.json())
+      // .then(json => console.log(json));
+      axios
+        .get("http://swapi.com/api/people")
+        .then(json => console.log(json))
+        .catch(response => console.log(response));
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
+window.axios = require("axios");
+
 new Vue({
   el: "#app",
   render: h => h(App)
